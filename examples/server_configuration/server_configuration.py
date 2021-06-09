232b6eea97bd8541ebc468585b2fe246130a8efe
Vikrant Balyan (vvb) <vvb@cisco.com>
2021-06-09 14:59:52 +0530

    adding example for server configuration

---
+++ examples/server_configuration/server_configuration.py
@@ -0,0 +1,11 @@
+# create a server profile
+
+# create a couple of server policies
+
+# attach the profile to policies
+
+# assign a server to the server profile
+
+# trigger deployment of the server profile
+
+
