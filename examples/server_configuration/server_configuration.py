0220df28ab09dfdb17d23856d2756d80706ad5ee
AKKI191289 <sam1232007@gmail.com>
2021-06-11 11:06:22 +0530

    [Server Configuration update] Work Done:
    create a server profile
    create a couple of server policies (created ntp, snmp, smtp)
    attach the profile to the policies using policy bucket.
    assign a server to the server profile.
    trigger deployment of the server profile.

--- examples/server_configuration/server_configuration.py
+++ examples/server_configuration/server_configuration.py
@@ -1,7 +1,7 @@
 from examples.authentication import client
 from intersight.model.server_profile import ServerProfile
 from intersight.api import server_api
-from intersight.model.policy_abstract_config_profile_relationship import PolicyAbstractConfigProfileRelationship
+from intersight.model.policy_abstract_policy_relationship import PolicyAbstractPolicyRelationship
 from intersight.model.ntp_policy import NtpPolicy
 from intersight.api import ntp_api
 from intersight.model.smtp_policy import SmtpPolicy
@@ -10,6 +10,9 @@ from intersight.model.snmp_policy import SnmpPolicy
 from intersight.api import snmp_api
 from intersight.model.snmp_user import SnmpUser
 from intersight.model.organization_organization_relationship import OrganizationOrganizationRelationship
+from intersight.model.compute_physical_relationship import ComputePhysicalRelationship
+from intersight.api import compute_api
+from intersight.exceptions import NotFoundException
 from pprint import pprint
 import intersight
 import sys
@@ -28,11 +31,11 @@ def create_organization():
     return organization
 
 
-def create_profile(server_profile_moid):
-    profile = PolicyAbstractConfigProfileRelationship(moid=server_profile_moid,
-                                                      object_type="server.Profile",
-                                                      class_id="mo.MoRef")
-    return profile
+def create_policy(policy_moid, obj_type):
+    policy = PolicyAbstractPolicyRelationship(moid=policy_moid,
+                                              object_type=obj_type,
+                                              class_id="mo.MoRef")
+    return policy
 
 
 def create_server_profile():
@@ -60,7 +63,7 @@ def create_server_profile():
         sys.exit(1)
 
 
-def create_ntp_policy(server_profile_moid):
+def create_ntp_policy():
     api_instance = ntp_api.NtpApi(api_client)
 
     # Create an instance of organization.
@@ -74,11 +77,9 @@ def create_ntp_policy(server_profile_moid):
     ntp_policy.set_attribute("description", "sample ntp policy.")
     ntp_policy.set_attribute("organization", organization)
     ntp_servers = [
-       "10.10.10.250", "10.10.10.10", "10.10.10.20", "10.10.10.30"
+        "10.10.10.250", "10.10.10.10", "10.10.10.20", "10.10.10.30"
     ]
     ntp_policy.set_attribute("ntp_servers", ntp_servers)
-    profile = create_profile(server_profile_moid)
-    ntp_policy.set_attribute("profiles", [profile])
 
     # example passing only required values which don't have defaults set
     try:
@@ -91,7 +92,7 @@ def create_ntp_policy(server_profile_moid):
         sys.exit(1)
 
 
-def create_smtp_policy(server_profile_moid):
+def create_smtp_policy():
     # Create an instance of the API class
     api_instance = smtp_api.SmtpApi(api_client)
 
@@ -109,8 +110,6 @@ def create_smtp_policy(server_profile_moid):
     smtp_policy.set_attribute("smtp_recipients", ["test@test"])
     smtp_policy.set_attribute("smtp_server", "10.10.10.80")
     smtp_policy.set_attribute("organization", organization)
-    profile = create_profile(server_profile_moid)
-    smtp_policy.set_attribute("profiles", [profile])
 
     # example passing only required values which don't have defaults set
     try:
@@ -123,7 +122,7 @@ def create_smtp_policy(server_profile_moid):
         sys.exit(1)
 
 
-def create_snmp_policy(server_profile_moid):
+def create_snmp_policy():
     # Create an instance of the API class
     api_instance = snmp_api.SnmpApi(api_client)
 
@@ -153,17 +152,105 @@ def create_snmp_policy(server_profile_moid):
                          )
     snmp_policy.set_attribute("snmp_users", [snmp_user])
     snmp_policy.set_attribute("organization", organization)
-    profile = create_profile(server_profile_moid)
-    snmp_policy.set_attribute("profiles", [profile])
 
     # example passing only required values which don't have defaults set
     try:
-        # Create a 'smtp.Policy' resource.
+        # Create a 'Snmp.Policy' resource.
         api_response = api_instance.create_snmp_policy(snmp_policy)
         pprint(api_response)
         return api_response
     except intersight.ApiException as e:
-        print("Exception when calling SmtpApi->create_smtp_policy: %s\n" % e)
+        print("Exception when calling SnmpApi->create_snmp_policy: %s\n" % e)
+        sys.exit(1)
+
+
+def attach_server_profile(server_profile_moid):
+    api_instance = server_api.ServerApi(api_client)
+
+    # Creating the compute instance api.
+    compute_api_instance = compute_api.ComputeApi(api_client)
+
+    # Getting the list of compute rack unit.
+    compute_api_response = compute_api_instance.get_compute_rack_unit_list()
+
+    # Handling error scenario if get_compute_rack_unit_list does not return any entry.
+    if not compute_api_response.results:
+        raise NotFoundException(reason="The response does not contain any entry for compute rack unit."
+                                       "Please connect a compute rack unit and then attach the server profile.")
+
+    # Selecting the first compute rack unit to attach the server profile.
+    # Fetching the moid.
+    rack_unit_moid = compute_api_response.results[0].moid
+
+    # Creation of server profile model instance.
+    server_profile = ServerProfile()
+
+    # Creation of compute physical relationship object.
+    # This instance contains moid of chosen rack unit.
+    assigned_server = ComputePhysicalRelationship(class_id="mo.MoRef",
+                                                  object_type="compute.RackUnit",
+                                                  moid=rack_unit_moid)
+
+    # Setting the attribute for server profile with assigned server and server profile moid.
+    server_profile.set_attribute("assigned_server", assigned_server)
+
+    # example passing only required values which don't have defaults set
+    try:
+        # Patching a 'Server.Profile' resource.
+        api_response = api_instance.patch_server_profile(moid=server_profile_moid,
+                                                         server_profile=server_profile)
+        pprint(api_response)
+        return api_response
+    except intersight.ApiException as e:
+        print("Exception when calling ServerApi->patch_server_profile: %s\n" % e)
+        sys.exit(1)
+
+
+def deploy_server_profile(server_profile_moid):
+    api_instance = server_api.ServerApi(api_client)
+
+    # Creation of server profile model instance.
+    server_profile = ServerProfile()
+
+    # Setting the attribute for server profile with the action and server profile moid.
+    server_profile.set_attribute("action", "Deploy")
+
+    # example passing only required values which don't have defaults set
+    try:
+        # Patching a 'Server.Profile' resource.
+        api_response = api_instance.patch_server_profile(moid=server_profile_moid,
+                                                         server_profile=server_profile)
+        pprint(api_response)
+        return api_response
+    except intersight.ApiException as e:
+        print("Exception when calling ServerApi->patch_server_profile: %s\n" % e)
+        sys.exit(1)
+
+
+def attach_policies_to_profile(policy_mapping, server_profile_moid):
+    policy_bucket = []
+
+    for obj_type, moid in policy_mapping.items():
+        policy = create_policy(moid, obj_type)
+        policy_bucket.append(policy)
+
+    api_instance = server_api.ServerApi(api_client)
+
+    # ServerProfile | The 'server.Profile' resource to create.
+    server_profile = ServerProfile()
+
+    # Setting all the attributes for server_profile instance.
+    server_profile.set_attribute("policy_bucket", policy_bucket)
+
+    # example passing only required values which don't have defaults set
+    try:
+        # Patch a 'server.Profile' resource.
+        api_response = api_instance.patch_server_profile(moid=server_profile_moid,
+                                                         server_profile=server_profile)
+        pprint(api_response)
+        return api_response
+    except intersight.ApiException as e:
+        print("Exception when calling ServerApi->patch_server_profile: %s\n" % e)
         sys.exit(1)
 
 
@@ -173,17 +260,29 @@ server_profile_response = create_server_profile()
 # Finding out server profile moid
 server_profile_moid = server_profile_response.moid
 
-# create a couple of server policies and attach the profile to the policies.
+# create a couple of server policies
 
 # Creating NTP policy
-ntp_policy_response = create_ntp_policy(server_profile_moid)
+ntp_policy_response = create_ntp_policy()
 
 # Creating SMTP policy
-smtp_policy_response = create_smtp_policy(server_profile_moid)
+smtp_policy_response = create_smtp_policy()
 
 # Creating SNMP policy
-snmp_policy_response = create_snmp_policy(server_profile_moid)
+snmp_policy_response = create_snmp_policy()
+
+# Creating a policy mapping with object Type as key and moid will be value.
+policies = {
+    "ntp.Policy": ntp_policy_response.moid,
+    "smtp.Policy": smtp_policy_response.moid,
+    "snmp.Policy": snmp_policy_response.moid,
+}
+
+# attach the profile to policies
+attach_policies_to_profile(policies, server_profile_moid)
 
 # assign a server to the server profile
+attach_server_profile(server_profile_moid)
 
 # trigger deployment of the server profile
+deploy_server_profile(server_profile_moid)
